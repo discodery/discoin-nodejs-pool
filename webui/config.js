08340c40f0150262336c49bb29b38268751bd56d
Florent <florent@discodery.com>
2019-09-18 08:53:58 +0000

    Initial commit

---
+++ webui/config.js
@@ -0,0 +1,16 @@
+var api = "https://discoin-mining-pool.discodery.com/api";
+var poolHost = "discoin-mining-pool.discodery.com";
+
+var email = "discoin@discodery.com";
+var telegram = "https://t.me/discoin_group";
+var discord = "https://discord.gg/BPWgmf4";
+
+var marketCurrencies = ["{symbol}-BTC"];
+
+var blockchainExplorer = "http://discoin-explorer.discodery.com/?hash={id}#blockchain/block";
+var transactionExplorer = "http://discoin-explorer.discodery.com/?hash={id}#blockchain/transaction";
+
+var themeCss = "themes/default.css";
+
+var langs = { 'en': 'English', 'fr': 'Français' };
+var defaultLang = 'en';
